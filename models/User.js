93e5d202f23aabcef4f7a0363288bf7281f9f658
dom971 <dom971@wanadoo.fr>
2026-05-10 17:13:44 -0400

    202605101713

--- models/User.js
+++ models/User.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  nom: { type: String, required: true },
-  isVerified: { type: Boolean, default: false },
-  verificationToken: { type: String }
+  nom: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
